264402d551489fffe1a336a31fd917331a2f08fc
peterbaumert <peter.baumert+github@outlook.com>
2023-06-07 20:37:04 +0200

    make it 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="netbox-device-view",
-    version="0.2.0-alpha",
+    version="0.1.0",
     description="NetBox Device View plugin",
     packages=find_packages(),
     author="Peter Baumert",
