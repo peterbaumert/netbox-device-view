b025fe5e458c8d2c32a5cbf6fe109ca40187c81e
Peter Baumert <peter@baumert.io>
2026-03-13 17:55:09 +0100

    chore: add devcontainer, ruff linting, and dev tooling

---
+++ .devcontainer/extra.py
@@ -0,0 +1,9 @@
+PLUGINS = ["netbox_device_view"]
+
+PLUGINS_CONFIG = {
+    "netbox_device_view": {
+        "show_on_device_tab": False,
+    }
+}
+
+DEVELOPER = True  # allows makemigrations; safe in devcontainer only
